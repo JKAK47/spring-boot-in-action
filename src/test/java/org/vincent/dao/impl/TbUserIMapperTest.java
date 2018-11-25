bf3876af67d899d063fb4aec6f980c331dec84c0
PR <1557862201@qq.com>
2018-11-26 01:23:03 +0800

    关于mybatis 自动生成代码的MBG配置文件配置

--- src/test/java/org/vincent/dao/impl/TbUserIMapperTest.java
+++ src/test/java/org/vincent/dao/impl/TbUserIMapperTest.java
@@ -1,11 +1,14 @@
 package org.vincent.dao.impl;
 
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.vincent.dao.imapper.TbUserIMapper;
+import org.vincent.dao.model.BatchJobInstance;
+import org.vincent.dao.model.BatchJobInstanceExample;
 import org.vincent.dao.model.TbUser;
 
 /**
@@ -17,6 +20,8 @@ public class TbUserIMapperTest {
     @Autowired
     private TbUserIMapper tbUserIMapper;
 
+    @Autowired
+    private  BatchJobInstanceMapper batchJobInstanceMapper;
     /**
      *  一开始 执行 报错没找到对应的org.vincent.dao.imapper.TbUserIMapper.getTbUser 是因为 映射xml 文件 TbUserICustomMapper 没有文件
      *  后缀名 .xml ; 只需要添加上后就好啦。
@@ -25,5 +30,12 @@ public class TbUserIMapperTest {
     public void  testgetTbUsers(){
         TbUser lists=tbUserIMapper.getTbUser(1);
         System.out.println(lists.toString());
+    }
+    @Test
+    public  void  testselectByExample(){
+        BatchJobInstanceExample example =new  BatchJobInstanceExample();
+        example.createCriteria().andJobInstanceIdEqualTo(1L);
+        List<BatchJobInstance> instances = batchJobInstanceMapper.selectByExample(example);
+        System.out.println(instances.size());
     }
 }
